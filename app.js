288e80324ceb2cbc5fe32da6476f8464c6b47d73
domagojl <96523928+domagojl@users.noreply.github.com>
2022-03-21 19:44:44 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var start = document.getElementById('start');
+var reset = document.getElementById('reset');
+
+var h = document.getElementById('hour');
+var m =document.getElementById('minute');
+var s = document.getElementById('sec');
+
+
+//store a reference to the variable
+
+var startTimer = null; 
+
+function timer(){
+    if(h.value == 0 && m.value == 0 && s.value == 0){
+        h.value = 0;
+        s.value = 0;
+        m.value = 0;
+    } else if (s.value != 0){
+        s.value --;
+    } else if (m.value != 0 && s.value == 0){
+        s.value = 59;
+        m.value --;
+    } else if (h.value != 0 && m.value == 0 && s.value == 0){
+        s.value = 59;
+        m.value = 59;
+        h.value --;
+    }
+    return;
+}
+
+
+function stopTimer(){
+    clearInterval(startTimer);
+}
+
+
+
+start.addEventListener('click', function() {
+    //initialize the variable starttimer
+
+    function startInterval(){
+        startTimer = setInterval(function(){
+            timer();
+        }, 1000);
+    }
+    startInterval()
+})
+
+
+reset.addEventListener('click', function(){
+    h.value = 0;
+    m.value = 0;
+    s.value = 0;
+
+    stopTimer()
+})
